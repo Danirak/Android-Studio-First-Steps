0fd8eac079935e0122a54ed81e3537296b3d9206
Diego Ortega <11.diego.ortega@gmail.com>
2025-03-31 17:32:26 -0300

    Pruebas del video hasta 3:55:30

---
+++ app/src/main/java/com/example/primeraprueba/firstapp/FirstAppActivity.kt
@@ -0,0 +1,30 @@
+package com.example.primeraprueba.firstapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.primeraprueba.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        btnStart.setOnClickListener {
+            Log.i("Devss", "${etName.text.toString()}")
+        }
+    }
+}
